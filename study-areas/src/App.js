c697e38a4f360d4f64ce09a7bf4d4c565217be79
celimardeida <celimar.deida@upr.edu>
2022-10-26 21:10:17 -0400

    modifications to catalog page

--- study-areas/src/App.js
+++ study-areas/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Catalog from './components/Catalog';
 import FindClassroom from './components/Recommender';
--- study-areas/src/components/Catalog.js
+++ study-areas/src/components/Catalog.js
@@ -1,6 +1,12 @@
 import {useEffect, useState} from 'react'
 import Classroom from './Classroom'
 import './Catalog.css'
+import map from './uprm_map.png';
+import mapkey1 from './mapkey1.png';
+import mapkey2 from './mapkey2.png';
+import mapkey3 from './mapkey3.png';
+import mapkey4 from './mapkey4.png';
+
 
 const Catalog = () => {
     const [data, setData] = useState([]);
@@ -12,6 +18,11 @@ const Catalog = () => {
     return (
         <div>
             <h1 className="title">Catalog</h1>
+            <img src={map} className="campus-map" />
+            <img src={mapkey1} className="map-key" />
+            <img src={mapkey2} className="map-key" />
+            <img src={mapkey3} className="map-key" />
+            <img src={mapkey4} className="map-key" />
             <div className="classrooms">
                 {data.map((classroom) => (
                     <Classroom code={classroom.code} building={classroom.building} image={classroom.img}/>
