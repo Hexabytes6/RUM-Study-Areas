eb88e8a558b65705a0ad2aba6b1f8dbae4bfe1da
celimardeida <celimar.deida@upr.edu>
2022-10-31 23:57:28 -0400

    header picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Header.css'
+import paw from './paw.webp';
 
 const Header = () => {
     return (
         <div className="header">
+            <img src={paw} className="paw-logo" />
             <h1 className="logo">Study Areas</h1>
             <div className="links">
                 <Link to="/recommender">Classroom Recommendation</Link>
